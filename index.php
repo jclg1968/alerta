10e754e89c924503c221e616f1bc9e22955fc1f8
jclg <jclg@mercurio.(none)>
2014-05-06 20:58:11 -0430

    tercer commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	if(isset($_POST['json'])){
 		$json = json_decode($_POST['json']);
-		echo "El idIns es: ".$json->type.",<br>su Latitud es: ".$json->lat."<br>su Longitud es: ".$json->lng.".<br>su telefono es : ".$json->phone.",<br><br> Mientras procesamos su solicitud ayudenos a tipificar su incidencia, llenando el formulario a continuacion";
+		echo "El Numero de Incidencia es: ".$json->type.",<br>su Latitud es: ".$json->lat."<br>su Longitud es: ".$json->lng.".<br>su telefono es : ".$json->phone.",<br><br> Mientras procesamos su solicitud ayudenos a tipificar su incidencia, llenando el formulario a continuacion";
 		// echo "Mientras procesamos su solicitud ayudenos a tipificar su incidencia, llenando el formulario a continuacion";
 	} else {
 		echo "no fue post";
--- js/index.js
+++ js/index.js
@@ -1,45 +1,53 @@
-$('.btn-home').click(function(event){
+$(document).on('click', '.btn-home', function(event) { 
 	event.preventDefault();
 	var id = $(this).attr("id");
-	var lat, lng;
+	var lat, lng, nombre, telefono, email;
 	navigator.geolocation.getCurrentPosition(function(position)
 	{ 
 	    lat = position.coords.latitude;
 	    lng = position.coords.longitude;
-
+	    nombre = localStorage.nombre;
+	    telefono = localStorage.telefono;
+	    email = localStorage.email;
 	    switch(id){
 			case 'policia':
-				var jsonData = JSON.stringify({"type": "1", "lat": lat, "lng": lng, "phone":"04127305740" });
+				var jsonData = JSON.stringify({"type": "1", "lat": lat, "lng": lng, "phone":telefono, "nombre":nombre, "email":email });
 			break;	
 			case 'ambulancia':
-				var jsonData = JSON.stringify({"type": "2", "lat": lat, "lng": lng, "phone":"04127305740"  });
+				var jsonData = JSON.stringify({"type": "2", "lat": lat, "lng": lng, "phone":telefono, "nombre":nombre, "email":email });
 			break;	
 			case 'mecanico':
-				var jsonData = JSON.stringify({"type": "3", "lat": lat, "lng": lng, "phone":"04127305740"  });
+				var jsonData = JSON.stringify({"type": "3", "lat": lat, "lng": lng, "phone":telefono, "nombre":nombre, "email":email });
 			break;	
 			case 'bombero':
-				var jsonData = JSON.stringify({"type": "4", "lat": lat, "lng": lng, "phone":"04127305740"  });
+				var jsonData = JSON.stringify({"type": "4", "lat": lat, "lng": lng, "phone":telefono, "nombre":nombre, "email":email });
 			break;
 		};	
 		$.ajax({
 			type: "POST",
-			url: 'http://192.168.1.139/our_mapper/assets/api.php',
-			//url: 'index.php',
+			//url: 'http://192.168.1.139/our_mapper/assets/api.php',
+			url: 'index.php',
 			crossDomain:true,             
         	async: true,
 			data:'json='+jsonData ,
-			success: function( data ) {    
-			alert(data);         			
-				//$('#respuestaServidor').html(data);
-				//$('#formulario').append($('#form1'));
-				//$.mobile.changePage( "#popupDialog", { role: "dialog" });
+			success: function( data ) {
+				/*
+				var obj = jQuery.parseJSON(data);
+				var direccion = obj.direccion;
+				var id 		= obj.id;
+				var mitexto = "El id de incidencia es "+id+" Ud esta ubicado en "+direccion+".";
+				$('#respuestaServidor').html(mitexto);*/
+				$('#formulario').append($('#form1'));
+				$.mobile.changePage( "#popupDialog", {	rel: "dialog", 
+					    								transition: "flip"
+					    							});
 			}
 		});
 	});
 });
 
 $(document).on('click', '#submit', function() { 
-	alert('se envio el formulario');
+	//traemos los datos almacenados en local storage
     $.ajax({
     	url: 'http://192.168.1.139/our_mapper/assets/api.php',
 		crossDomain:true,
@@ -56,13 +64,31 @@ $(document).on('click', '#submit', function() {
         },
         success: function (result) {
                 resultObject.formSubmitionResult = result;
-                            $.mobile.changePage("#second");
+                //$.mobile.changePage("#second");
         },
         error: function (request,error) {
             // This callback function will trigger on unsuccessful action                
             alert('Network error has occurred please try again!');
         }
     });                   
-               
+            
+    $('[data-role=dialog]').dialog( "close" );   
     return false; // cancel original event to prevent form submitting
 }); 
+
+$(document).on('click', '#submit-personal-data', function() {     
+    localStorage.nombre = $('#nombre').val();
+    localStorage.telefono = $('#telefono').val();
+    localStorage.email = $('#email').val();
+    $.mobile.changePage("#popupConfirm", {	rel: "dialog", 
+		    								transition: "flip"
+		    							});
+}); 
+
+$(document).on('click', '#opt-configurar', function() {  
+    $('#nombre').val(localStorage.nombre);
+    $('#telefono').val(localStorage.telefono);
+    $('#email').val(localStorage.email);
+}); 
+
+
